0bb32342b1d5bc4fc49f96085213fb1a770c38b8
ColmofWar <joshuaallendimarco@gmail.com>
2024-09-11 13:14:26 -0400

    Site functional, unittest not functional

--- models.py
+++ models.py
@@ -1,1 +1,67 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+db = SQLAlchemy()
+
+
+DEFAULT_IMAGE = "https://tinyurl.com/demo-cupcake"
+
+class Base(DeclarativeBase):
+    pass
+
+"""
+class Cupcake(db.Model):
+    ""Cupcake.""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def to_dict(self):
+        ""Serialize cupcake to a dict of cupcake info.""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
+"""
+
+
+
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = "cupcakes"
+
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True, autoincrement=True)
+    flavor: Mapped[str] = mapped_column(db.Text, nullable=False)
+    size: Mapped[str] = mapped_column(db.Text, nullable=False)
+    rating: Mapped[int] = mapped_column(db.Float, nullable=False)
+    image: Mapped[str] = mapped_column(db.Text, nullable=False, default=DEFAULT_IMAGE)
+
+    def to_dict(self):
+        """Serialize cupcake to a dict of cupcake info."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "rating": self.rating,
+            "size": self.size,
+            "image": self.image,
+        }
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
--- tests.py
+++ tests.py
@@ -1,17 +1,18 @@
-from unittest import TestCase
+import unittest
 
 from app import app
 from models import db, Cupcake
 
 # Use test database and don't clutter tests with SQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes_test.db'
 app.config['SQLALCHEMY_ECHO'] = False
 
 # Make Flask errors be real errors, rather than HTML pages with error info
 app.config['TESTING'] = True
 
-db.drop_all()
-db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
 
 
 CUPCAKE_DATA = {
@@ -29,24 +30,27 @@ CUPCAKE_DATA_2 = {
 }
 
 
-class CupcakeViewsTestCase(TestCase):
+class CupcakeViewsTestCase(unittest.TestCase):
     """Tests for views of API."""
-
     def setUp(self):
         """Make demo data."""
-
-        Cupcake.query.delete()
-
+        with app.app_context():
+            Cupcake.query.delete()
         cupcake = Cupcake(**CUPCAKE_DATA)
-        db.session.add(cupcake)
-        db.session.commit()
+
+        with app.app_context():
+            db.session.add(cupcake)
+            db.session.commit()
 
         self.cupcake = cupcake
+        print(cupcake.id)
+       
+        
 
     def tearDown(self):
         """Clean up fouled transactions."""
-
-        db.session.rollback()
+        with app.app_context():
+            db.session.rollback()
 
     def test_list_cupcakes(self):
         with app.test_client() as client:
@@ -66,9 +70,10 @@ class CupcakeViewsTestCase(TestCase):
                     }
                 ]
             })
-
+    """
     def test_get_cupcake(self):
         with app.test_client() as client:
+
             url = f"/api/cupcakes/{self.cupcake.id}"
             resp = client.get(url)
 
@@ -83,6 +88,13 @@ class CupcakeViewsTestCase(TestCase):
                     "image": "http://test.com/cupcake.jpg"
                 }
             })
+
+    def test_get_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/99999"
+            resp = client.get(url)
+
+            self.assertEqual(resp.status_code, 404)
 
     def test_create_cupcake(self):
         with app.test_client() as client:
@@ -107,3 +119,50 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+
+    def test_update_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.patch(url, json=CUPCAKE_DATA_2)
+
+            self.assertEqual(resp.status_code, 200)
+
+            data = resp.json
+            self.assertEqual(data, {
+                "cupcake": {
+                    "id": self.cupcake.id,
+                    "flavor": "TestFlavor2",
+                    "size": "TestSize2",
+                    "rating": 10,
+                    "image": "http://test.com/cupcake2.jpg"
+                }
+            })
+
+            self.assertEqual(Cupcake.query.count(), 1)
+
+    def test_update_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/99999"
+            resp = client.patch(url, json=CUPCAKE_DATA_2)
+
+            self.assertEqual(resp.status_code, 404)
+
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+
+            self.assertEqual(resp.status_code, 200)
+
+            data = resp.json
+            self.assertEqual(data, {"message": "Deleted"})
+
+            self.assertEqual(Cupcake.query.count(), 0)
+
+    def test_delete_cupcake_missing(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/99999"
+            resp = client.delete(url)
+
+            self.assertEqual(resp.status_code, 404)
+"""
